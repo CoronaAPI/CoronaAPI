73861ebf7ab9b8ec6408df2f9915c444f7a89506
Benedikt <brauls101@gmail.com>
2020-03-21 11:39:33 +0100

    return the scraped data from today as an example for now

--- routes.js
+++ routes.js
@@ -1,14 +1,25 @@
+const fs = require("fs");
+
+function readJsonFileSync(filepath, encoding){
+  if (typeof (encoding) == 'undefined'){
+      encoding = 'utf8';
+  }
+  var file = fs.readFileSync(filepath, encoding);
+  return JSON.parse(file);
+}
+
 module.exports.setup = function(app) {
   /**
    * @swagger
    * /:
    *   get:
-   *     description: Just an example that will be replaced later
+   *     description: Get the scraped Corona data (example)
    *     responses:
    *       200:
    *         description: tbd
    */
   app.get("/", (req, res) => {
-    res.json({ message: "Corona REST API" });
+    const scrapedData = readJsonFileSync(__dirname + '/data.json')
+    res.json(scrapedData);
   });
 };
