af9587ce25ea717153c014a4908c0a6aef6a8120
ndo@ndo3 <yo@ni.co.de>
2020-03-21 11:13:26 +0100

    update: new branch

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# corona-api
-Begin app
+# Corona API
+
+> #WirVsVirus Project #0193
+
+HTTP Corona API based on the great work of [lazd/coronadatascraper](https://github.com/lazd/coronadatascraper).
+
+[DevPost Project](https://devpost.com/software/1_038_a_daten_0193_coronaapi)
+
+Automatically deployed via `begin.com` to [https://dance-uht-staging.begin.app/](https://dance-uht-staging.begin.app/)
+
+## Usage
+
+1. `git clone https://github.com/CoronaAPI/CoronaAPI`
+
+2. `cd CoronaAPI`
+
+3. `npm install`
+
+4. `npm start`
+
+The API will then be listening on `localhost:3333`
+
+## Contributors
+
+![brauls](https://avatars.githubusercontent.com/u/6483465?v=2&s=128)
+![ndom91](https://avatars.githubusercontent.com/u/7415984?v=2&s=128)
+
+## License
+
+MIT
--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,8 +1,39 @@
-let arc = require('@architect/functions')
-let express = require('express')
-let app = express()
+let arc = require("@architect/functions");
+let express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+let app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/cool', (req, res)=> res.send('very cool'))
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
 
-exports.handler = arc.http.express(app)
+// Beispiel whitelist
+// const whitelist = ['https://domain.de']
+const whitelist = ["*"];
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (process.env.NODE_ENV === "dev") {
+      callback(null, true);
+    } else {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    }
+  }
+};
+
+app.get("/", (req, res) => {
+  res.json({ message: "Corona REST API" });
+});
+
+app.get("/cool", (req, res) => res.send("very very cool"));
+
+app.get("/test", cors(corsOptions), (req, res) => {
+  res.status(200).sendFile(__dirname + "/package.json");
+});
+
+exports.handler = arc.http.express(app);
