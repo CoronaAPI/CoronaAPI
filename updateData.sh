6a99b9d13735af7131bab35965b5ea13b74c365e
ndom91 <yo@ndo.dev>
2020-03-24 16:02:22 +0000

    update: updateData.sh

--- updateData.sh
+++ updateData.sh
@@ -7,6 +7,7 @@
 TIME=$(date +%b-%d-%y)
 DATE=$(date +%Y-%m-%d)
 DIR="/opt/corona-api/data"
+export node=/home/ubuntu/.nvm/versions/node/v12.16.1/bin/node
 
 ##################
 # SETUP
@@ -28,11 +29,20 @@ echo "[*] Repo successfully cloned"
 
 cd coronadatascraper
 echo "[*] Installing coronadatascraper..."
-sudo -u ubuntu yarn install
-echo "[*] Starting coronadatascraper..."
-sudo -u ubuntu yarn start
 
-sudo -u ubuntu cp $DIR/$DATE/coronadatascraper/dist/data.json $DIR/$DATE
+# HACK UNTIL KS SOURCE IS REMOVED
+if [ -d "$DIR/$DATE/coronadatascraper/src/events/crawler/scrapers/USA/KS" ]
+then
+  rm -r $DIR/$DATE/coronadatascraper/src/events/crawler/scrapers/USA/KS
+fi
+# END HACK
+
+yarn install
+echo "[*] Starting coronadatascraper..."
+# sudo -u ubuntu /home/ubuntu/.nvm/versions/node/v12.16.1/bin/node src/events/crawler/cli.js
+yarn start
+
+cp $DIR/$DATE/coronadatascraper/dist/data.json $DIR/$DATE
 echo "[*] Data successfully dumped " + $TIME
 
 ##################
@@ -43,7 +53,7 @@ echo ""
 echo "[*] Cleaning up data scrape"
 
 cd $DIR/$DATE
-rm -r coronadatascraper
+# rm -r coronadatascraper
 
 echo "[*] Daily Script Complete!"
 
