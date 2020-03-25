a3cea5c628edc55c1bca618be2b109f303ae6a2d
ndom91 <yo@ndo.dev>
2020-03-25 15:13:30 +0000

    fix: variables in Slack webhook POST body

--- updateData.sh
+++ updateData.sh
@@ -62,7 +62,7 @@ rm -rf coronadatascraper
 
 if [ -e "$DIR/$DATE/data.json" ]
 then
-  curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"🚀 API source data updated at *$TIME*"}},{"type":"divider"},{"type":"context","elements":[{"type":"mrkdwn","text":"For more info, checkout ssh://$HOSTNAME"}]}]}' https://hooks.slack.com/services/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  >> /dev/null 2>&1
+  curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"🚀 API source data updated at *$TIME*\"}},{\"type\":\"divider\"},{\"type\":\"context\",\"elements\":[{\"type\":\"mrkdwn\",\"text\":\"For more info, checkout ssh://$HOSTNAME\"}]}]}" https://hooks.slack.com/services/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  >> /dev/null 2>&1
 fi
 
 echo "[*] Daily Script Complete!"
