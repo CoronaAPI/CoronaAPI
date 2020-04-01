c514983d89eb6920a876e9eb1748efb3c534ebcc
ndom91 <yo@ndo.dev>
2020-04-01 08:59:35 +0000

    update: addDynamo data script

--- addDateForDynamo.js
+++ addDateForDynamo.js
@@ -6,6 +6,7 @@ const year = today.getFullYear()
 const month = `${today.getMonth() + 1}`.padStart(2, 0)
 const day = `${today.getDate()}`.padStart(2, 0)
 const stringDate = [year, month, day].join("-")
+const fileDate = [year, month, day].join("")
 
 fs.readFile(`/opt/corona-api/data/${stringDate}/data.json`, 'utf8', (err, data) => {
   if (err) throw err;
@@ -13,8 +14,8 @@ fs.readFile(`/opt/corona-api/data/${stringDate}/data.json`, 'utf8', (err, data) 
   fileContent.forEach(entry => {
     entry.date = stringDate
   })
-  fs.writeFileSync(`/opt/corona-api/data/${stringDate}/data-aws.json`, JSON.stringify(fileContent))
-  exec(`aws s3 cp /opt/corona-api/data/${stringDate}/data-aws.json s3://corona-api-json-content`, (err, stdout, stderr) => {
+  fs.writeFileSync(`/opt/corona-api/data/${stringDate}/data-aws-${fileDate}.json`, JSON.stringify(fileContent))
+  exec(`aws s3 cp /opt/corona-api/data/${stringDate}/data-aws-${fileDate}.json s3://corona-api-json-content`, (err, stdout, stderr) => {
     if (err) { console.error(err) }
   })
 })
